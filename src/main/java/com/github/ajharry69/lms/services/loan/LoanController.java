80bb739591ae3495ac68b4fe9c016c007821e95c
Orinda Harrison (Mitch) <mitchke@duck.com>
2025-03-24 08:52:38 +0300

    Configure endpoint for registering client

--- src/main/java/com/github/ajharry69/lms/services/loan/LoanController.java
+++ src/main/java/com/github/ajharry69/lms/services/loan/LoanController.java
@@ -5,15 +5,24 @@ import com.github.ajharry69.lms.services.loan.model.LoanResponse;
 import com.github.ajharry69.lms.services.loan.model.LoanStatusResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/loans")
 @RequiredArgsConstructor
+@Slf4j
 public class LoanController {
     private final LoanService loanService;
+
+    @PostMapping("/client-registration")
+    public void clientRegistrationCallback(@RequestBody Map<String, Object> request) {
+        log.info("Received request: {}", request);
+    }
 
     @PostMapping
     public ResponseEntity<LoanResponse> requestLoan(@Valid @RequestBody LoanRequest request) {
--- src/main/java/com/github/ajharry69/lms/services/loan/model/RegisterClientRequest.java
+++ src/main/java/com/github/ajharry69/lms/services/loan/model/RegisterClientRequest.java
@@ -11,7 +11,7 @@ public record RegisterClientRequest(
 ) {
     public RegisterClientRequest(ServerUrlProvider urlProvider, LmsProperties properties) {
         this(
-                urlProvider.getServerUrl() + "/api/v1/transactions",
+                urlProvider.getServerUrl() + "/api/v1/loans/client-registration",
                 "LMS",
                 properties.username(),
                 properties.password()
